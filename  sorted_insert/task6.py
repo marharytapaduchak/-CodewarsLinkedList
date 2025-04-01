6018382badd479f48a62b0e4dc8ffa5fe0141156
Marharyta <paduchak.pn@ucu.edu.ua>
2025-04-01 19:58:40 +0300

    6 task completed "Linked Lists - Sorted Insert". Attached result from the platform

---  sorted_insert/task6.py
+++  sorted_insert/task6.py
@@ -1,13 +1,24 @@
 """Linked Lists - Sorted Insert"""
 class Node(object):
     """class Node"""
-    def __init__(self, data):
+    def __init__(self, data, next = None):
         self.data = data
-        self.next = None
+        self.next = next
 
 
 def sorted_insert(head, data):
     """func sorted_insert"""
-    # Your code goes here.
-    # Make sure to return the head of the list.
-    ...
+    temp = Node(data)
+    if head is None:
+        return temp
+    if data < head.data:
+        temp.next = head
+        return temp
+
+    node = head
+    while node.next is not None and node.next.data < data:
+        node = node.next
+
+    temp.next = node.next
+    node.next = temp
+    return head
